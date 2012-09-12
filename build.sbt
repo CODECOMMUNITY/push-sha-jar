20a7e7b9839b6af697214304f17117aecc715d7e
Josh Suereth <joshua.suereth@gmail.com>
2012-09-12 12:05:49 -0400

    Implemented push feature for jar file.

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "push"
 
 organization := "org.scala-lang.tools"
 
-libraryDependencies += "net.databinder" % "dispatch_2.9.2" % "0.8.8"
+libraryDependencies += "net.databinder" % "dispatch-http_2.9.2" % "0.8.8"
 
 scalaVersion := "2.9.2"
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,43 @@
+import java.io.File
 
+case class Credentials(user: String, pw: String)
+case class Arguments(jar: File, uri: String, cred: Credentials)
 
 object Main extends App {
-  println("O HAI")
+
+  def usage(err: Option[String] = None): Nothing = {
+    err foreach (msg => println("\t" + msg))
+    println("""| Usage: java -jar push.jar <file> <remote url> <user> <encrypted pw>
+               |
+               |  This utility is used to push JAR files to a maven/ivy repository.
+               |""".stripMargin)
+    throw new Exception()
+  }
+  
+  def parseArgs: Arguments = {
+    if(args.length != 4) usage()
+    val file = Option(new File(args(0))) filter (_.isFile) getOrElse usage(Some(args(0) + " is not a file!"))
+    val uri = Option(args(1)) getOrElse usage(Some("uri is empty"))
+    val user = Option(args(2)) getOrElse usage(Some("user is empty"))
+    val pw = Option(args(3)) getOrElse usage(Some("password is empty"))
+    Arguments(file, uri, Credentials(user, pw))
+  }
+  
+  // Pushes a file and writes the new .desired.sha1 for git.
+  def pushFile(file: File, uri: String, cred: Credentials): Unit = {
+    val url = uri
+    val sender = dispatch.url(uri).PUT.as(cred.user,cred.pw) <<< (file, "application/java-archive")
+    // TODO - output to logger.
+    dispatch.Http(sender >>> System.out)
+  }
+  
+  
+  try {
+    val a =  parseArgs
+    pushFile(a.jar, a.uri, a.cred)
+  } catch {
+    case t: Exception => System.exit(1)
+  }
 }
+
+
